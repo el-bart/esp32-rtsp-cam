f9a9a06841056636a4e3cb7645e1db71dd13884b
bartek 'basz' szurgot <git@baszerr.eu>
2025-01-11 22:04:53 +0100

    basic WiFi handling code

--- sw/src/main.cpp
+++ sw/src/main.cpp
@@ -1,16 +1,23 @@
-#include <WiFi.h>
 #include "watchdog.hpp"
+#include "wifi.hpp"
 #include "wifi_creds.hpp"
 
 void setup()
 {
   Serial.begin(115200);
-
   Serial.println();
+  Serial.println("-----------");
   Serial.println("booting");
 
   watchdog_init();
   Serial.println("watchdog enabled");
+
+  wifi_init();
+  do
+  {
+    wifi_scan();
+  }
+  while(not wifi_connect(wifi_ssid, wifi_pass));
 
   // TODO
 }
---
+++ sw/src/wifi.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include <WiFi.h>
+#include "watchdog.hpp"
+#include "wifi_creds.hpp"
+
+inline void wifi_init()
+{
+  Serial.println("initializing WiFi");
+  WiFi.mode(WIFI_STA);  // client mode
+  WiFi.disconnect();
+}
+
+
+namespace detail
+{
+inline char const* enc_type(int i)
+{
+  switch( WiFi.encryptionType(i) )
+  {
+    case WIFI_AUTH_OPEN:            return "open network";
+    case WIFI_AUTH_MAX:             return "MAX";
+    case WIFI_AUTH_WAPI_PSK:        return "WAPI PSK";
+    case WIFI_AUTH_WEP:             return "WEP";
+    case WIFI_AUTH_WPA2_ENTERPRISE: return "WPA2 enterprise";
+    case WIFI_AUTH_WPA2_PSK:        return "WPA2 PSK";
+    case WIFI_AUTH_WPA2_WPA3_PSK:   return "WPA2/WPA3 PSK";
+    case WIFI_AUTH_WPA3_ENT_192:    return "WPA3 enterprise 192";
+    case WIFI_AUTH_WPA3_PSK:        return "WPA3 PSK";
+    case WIFI_AUTH_WPA_PSK:         return "WPA PSK";
+    case WIFI_AUTH_WPA_WPA2_PSK:    return "WPA/WPA2 PSK";
+  }
+  return "unknown";
+}
+} // namespace detail
+
+
+inline void wifi_scan()
+{
+  watchdog_reset();
+  Serial.println("scanning for nearby WiFi networks");
+  auto const n = WiFi.scanNetworks();
+  Serial.printf("WiFi scanning completed - got %d instances:\r\n", n);
+  watchdog_reset();
+
+  for(auto i=0; i<n; ++i)
+  {
+    auto const& ssid = WiFi.SSID(i);
+    auto const& enc = detail::enc_type(i);
+    auto const  rssi = WiFi.RSSI(i);
+    Serial.printf("%30s | %4d | %s\r\n", ssid.c_str(), rssi, enc);
+  }
+  Serial.println();
+
+  watchdog_reset();
+}
+
+
+inline bool wifi_connect(char const* ssid, char const* psk)
+{
+  Serial.printf("connecting to WiFi: %s\r\n", ssid);
+  return false; // TODO
+}
