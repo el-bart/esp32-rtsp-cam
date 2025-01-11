707a50cee8712b6a063ccaf876b341997305529d
bartek 'basz' szurgot <git@baszerr.eu>
2025-01-11 20:56:49 +0100

    dockerized SDK + dummy main

---
+++ sw/src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <esp_task_wdt.h>
+
+void setup()
+{
+  Serial.begin(115200);
+
+  Serial.println();
+  Serial.println("booting");
+
+  esp_task_wdt_init(5/*s*/, true);
+  esp_task_wdt_add(NULL);   // add current task to watchdog watch
+  Serial.println("watchdog enabled");
+
+  // TODO
+}
+
+void loop()
+{
+  Serial.println("i'm alive!");
+  esp_task_wdt_reset();
+  delay(1000);
+  // TODO
+}
