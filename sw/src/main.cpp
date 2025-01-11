41a38de5b86063db2379ff9a5f781084a2b88861
bartek 'basz' szurgot <git@baszerr.eu>
2025-01-11 22:06:43 +0100

    more visible boot line

--- sw/src/main.cpp
+++ sw/src/main.cpp
@@ -6,7 +6,7 @@ void setup()
 {
   Serial.begin(115200);
   Serial.println();
-  Serial.println("-----------");
+  Serial.println("----------------------------------");
   Serial.println("booting");
 
   watchdog_init();
