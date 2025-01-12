acba13f93df9149b06b205e106d40c2e9184de49
bartek 'basz' szurgot <git@baszerr.eu>
2025-01-12 11:22:50 +0100

    added RTSP to main + added exception handling in key places

--- sw/src/main.cpp
+++ sw/src/main.cpp
@@ -1,31 +1,60 @@
+#include <OV2640.h>
 #include "watchdog.hpp"
 #include "wifi.hpp"
 #include "wifi_creds.hpp"
+#include "RTSP_server.hpp"
+
+
+std::unique_ptr<RTSP_server> rtsp_server;
+OV2640 camera;
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("----------------------------------");
-  Serial.println("booting");
-
-  watchdog_init();
-  Serial.println("watchdog enabled");
-
-  wifi_init();
-  do
+  try
   {
-    wifi_scan();
-  }
-  while(not wifi_connect(wifi_ssid, wifi_pass));
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println("##################################");
+    Serial.println("booting");
 
-  // TODO
+    watchdog_init();
+    Serial.println("watchdog enabled");
+
+    Serial.println("initializing camera");
+    if( camera.init(esp32cam_config) != ESP_OK )
+      throw std::runtime_error{"setup(): failed to initialize camera"};
+
+    wifi_init();
+    do
+    {
+      wifi_scan();
+    }
+    while(not wifi_connect(wifi_ssid, wifi_pass));
+
+    Serial.printf("starting RTSP server on %s:%d\r\n", WiFi.localIP().toString().c_str(), RTSP_server::port());
+    rtsp_server.reset(new RTSP_server{camera});
+
+    Serial.println("initialization sequence completed!");
+    Serial.println("----------------------------------");
+  }
+  catch(std::exception const& ex)
+  {
+    Serial.printf("setup(): exception caught: %s\r\n", ex.what());
+    throw;
+  }
 }
 
 void loop()
 {
-  Serial.println("i'm alive!");
-  watchdog_reset();
-  delay(1000);
-  // TODO
+  try
+  {
+    watchdog_reset();
+    rtsp_server->update();
+    delay( 1000 / RTSP_server::fps() );
+  }
+  catch(std::exception const& ex)
+  {
+    Serial.printf("loop(): exception caught: %s\r\n", ex.what());
+    throw;
+  }
 }
